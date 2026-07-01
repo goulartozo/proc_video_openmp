f1505da01ce276fc5d92d325dc89c3118cf0de19
Bernardo Oliveira Bencke <bernardoobencke@gmail.com>
2026-07-01 20:29:55 -0300

    Ajustes MPI

--- mainProc.cpp
+++ mainProc.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <omp.h>
+#include <mpi.h>
 
 using namespace cv;
 
@@ -9,19 +10,54 @@ void processarVideo(VideoCapture& capture, int filtro, int intensidade);
 void aplicarConvolucao(Mat& frame, const float kernel[3][3]);
 void aplicarSharpen(Mat& frame, int intensidade);
 void aplicarEscalaDeCinza(Mat& frame);
+void aplicarEmboss(Mat& frame, int intensidade);
+void aplicarSobel(Mat& frame, int intensidade);
+
+void master(const char* nomeArquivo, int filtro, int intensidade, int size);
+void worker(int rank, int filtro, int intensidade);
 
 int main(int argc, char** argv)
 {
-    int opcao;
-    int intensidadeDoEfeito = 5;
+
+    MPI_Init(&argc, &argv);
+
+    int rank;
+    int size;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    printf("\nProcesso %d de %d inicializado.\n", rank, size);
 
     if (argc < 2)
     {
-        printf("Uso: %s <video> Ex: ./mainProc video2.avi\n", argv[0]);
-        return 1;
+        if (rank == 0)
+        {
+            printf("\nUso: %s <video> Ex: ./mainProc video2.avi\n", argv[0]);
+        }
+
+        MPI_Finalize();
+        return 0;
     }
 
-    while (true)
+    if (size < 2)
+    {
+        if (rank == 0)
+        {
+            printf("\n Execute com pelo menos 2 processos.\n");
+            printf("Ex: mpirun -np 4 ./mainProc video2.avi\n");
+        }
+
+        MPI_Finalize();
+        return 0;
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    int opcao = 1;
+    int intensidadeDoEfeito = 5;
+
+    if(rank == 0)
     {
         printf("\n===== FILTROS =====\n");
         printf("1 - Escala de Cinza\n");
@@ -30,39 +66,210 @@ int main(int argc, char** argv)
         printf("4 - Sobel\n");
         printf("0 - Sair\n");
         printf("Opcao: ");
+        fflush(stdout);
 
         scanf("%d", &opcao);
 
-        if (opcao == 0)
-            break;
-
-        if(opcao > 4)
+        if (opcao == 0){
+            printf("\nEncerrando o programa...\n");
+        }
+        else if(opcao < 1 || opcao > 4)
 		{
 			printf("\nOpção inválida! Tente novamente.\n");
-			continue;
+			opcao = 0;
 		}
-
-        if (opcao != 1)
+        else if (opcao != 1)
         {
             printf("\nQual intensidade? ");
+            fflush(stdout);
             scanf("%d", &intensidadeDoEfeito);
         }
-
-        VideoCapture capture = carregarVideo(argv[1]);
-
-        if (!capture.isOpened())
-        {
-            printf("\n\nErro ao abrir video: %s\n", argv[1]);
-            printf("Verifique se o arquivo existe e se o caminho está correto.\n");
-            continue;
-        }
-
-        processarVideo(capture, opcao, intensidadeDoEfeito);
-
-        capture.release();
     }
 
+    MPI_Bcast(&opcao, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&intensidadeDoEfeito, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (opcao == 0)
+    {
+        MPI_Finalize();
+        return 0;
+    }
+
+    printf("\nProcesso %d recebeu configuração -> Filtro: %d | Intensidade: %d\n", rank, opcao, intensidadeDoEfeito);
+
+    if (rank == 0)
+    {
+        printf("\nProcesso Master iniciando processamento do vídeo...\n");
+        master(argv[1], opcao, intensidadeDoEfeito, size);
+    }
+    else
+    {
+        printf("Processo Worker %d aguardando blocos de imagem...\n", rank);
+        worker(rank, opcao, intensidadeDoEfeito);
+    }
+
+    printf("Processo %d finalizado.\n", rank);
+    MPI_Finalize();
+
     return 0;
+}
+
+void master(const char* nomeArquivo, int filtro, int intensidade, int size)
+{
+    VideoCapture capture(nomeArquivo);
+
+    if (!capture.isOpened())
+    {
+        printf("\nErro ao abrir o vídeo.\n");
+        int fimErro[3] = {0, 0, 0};
+        MPI_Bcast(fimErro, 3, MPI_INT, 0, MPI_COMM_WORLD);
+        return;
+    }
+
+    printf("\nVídeo aberto com sucesso.\n");
+
+    Mat frame;
+
+    while (capture.read(frame))
+    {
+        int largura = frame.cols;
+        int altura = frame.rows;
+        int canais = frame.channels();
+
+        int info[3];
+        info[0] = largura;
+        info[1] = altura;
+        info[2] = canais;
+
+        MPI_Bcast(info, 3, MPI_INT, 0, MPI_COMM_WORLD);
+
+        int workers = size - 1;
+
+        int linhasBase = altura / workers;
+        int resto = altura % workers;
+
+        int linhaInicial = 0;
+
+        // Envia blocos para os workers
+        for (int destino = 1; destino < size; destino++)
+        {
+            int linhas = linhasBase;
+
+            if (destino <= resto)
+                linhas++;
+
+            int primeiraLinha = linhaInicial;
+            int ultimaLinha = linhaInicial + linhas - 1;
+
+            int inicioEnvio = primeiraLinha;
+            int fimEnvio = ultimaLinha;
+
+            if (inicioEnvio > 0)
+                inicioEnvio--;
+
+            if (fimEnvio < altura - 1)
+                fimEnvio++;
+
+            int linhasEnviar = fimEnvio - inicioEnvio + 1;
+
+            int dados[3];
+            dados[0] = primeiraLinha;
+            dados[1] = linhas;
+            dados[2] = linhasEnviar;
+
+            MPI_Send(dados, 3, MPI_INT, destino, 0, MPI_COMM_WORLD);
+
+            MPI_Send(frame.ptr(inicioEnvio), linhasEnviar * largura * canais, MPI_UNSIGNED_CHAR, destino, 1, MPI_COMM_WORLD);
+
+            printf("Master enviou %d linhas para Worker %d.\n", linhasEnviar, destino);
+
+            linhaInicial += linhas;
+        }
+
+        // Recebe os blocos processados
+        linhaInicial = 0;
+
+        for (int origem = 1; origem < size; origem++)
+        {
+            int linhas = linhasBase;
+
+            if (origem <= resto)
+                linhas++;
+
+            MPI_Recv(frame.ptr(linhaInicial), linhas * largura * canais, MPI_UNSIGNED_CHAR, origem, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            printf("Master recebeu %d linhas do Worker %d.\n", linhas, origem);
+
+            linhaInicial += linhas;
+        }
+
+        imshow("Video MPI + OpenMP", frame);
+
+        if ((waitKey(30) & 0xFF) == 27)
+            break;
+    }
+
+    int fim[3] = {0, 0, 0};
+
+    MPI_Bcast(fim, 3, MPI_INT, 0, MPI_COMM_WORLD);
+
+    printf("Master finalizando processamento.\n");
+
+    capture.release();
+
+    destroyAllWindows();
+}
+
+void worker(int rank, int filtro, int intensidade)
+{
+    while (true)
+    {
+        int info[3];
+        MPI_Bcast(info, 3, MPI_INT, 0, MPI_COMM_WORLD);
+
+        int largura = info[0];
+        int altura  = info[1];
+        int canais  = info[2];
+
+        // Sinal de término enviado pelo master ({0,0,0})
+        if (largura == 0 && altura == 0 && canais == 0)
+        {
+            printf("Worker %d recebeu sinal de término.\n", rank);
+            break;
+        }
+
+        int dados[3];
+        MPI_Recv(dados, 3, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        int primeiraLinha = dados[0];
+        int linhas        = dados[1];
+        int linhasEnviar  = dados[2];
+
+        // Bloco recebido já inclui as linhas de halo (se existirem)
+        Mat bloco(linhasEnviar, largura, CV_8UC3);
+
+        MPI_Recv(bloco.ptr(0), linhasEnviar * largura * canais, MPI_UNSIGNED_CHAR, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        switch (filtro)
+        {
+            case 1: aplicarEscalaDeCinza(bloco); break;
+            case 2: aplicarSharpen(bloco, intensidade); break;
+            case 3: aplicarEmboss(bloco, intensidade); break;
+            case 4: aplicarSobel(bloco, intensidade); break;
+            default:
+                printf("\nOpção inválida! Tente novamente.\n");
+                break;
+        }
+
+        // Se primeiraLinha > 0, o bloco tem 1 linha de halo no topo
+        int offsetTopo = (primeiraLinha == 0) ? 0 : 1;
+
+        // Devolve só as linhas "reais" (sem o halo)
+        MPI_Send(bloco.ptr(offsetTopo), linhas * largura * canais, MPI_UNSIGNED_CHAR,
+                   0, 2, MPI_COMM_WORLD);
+
+        printf("Worker %d processou e enviou %d linhas de volta ao Master.\n", rank, linhas);
+    }
 }
 
 VideoCapture carregarVideo(const char* nomeArquivo)
@@ -119,7 +326,7 @@ void aplicarSharpen(Mat& frame, int intensidade)
     float kernel[3][3] =
     {
         {  0, -1,  0 },
-        { -1, (float)intensidade, -1 },
+        { -1, 4.0f + (float)intensidade, -1 },
         {  0, -1,  0 }
     };
 
